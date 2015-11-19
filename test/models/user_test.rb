22e350eec8f5ac608df1f5c6d954b94e8368b98a
NoNameA 774 <nonamea774@gmail.com>
2015-11-19 16:38:43 +0000

    add little test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "OpenYo", email: "OpenYo@nna774.net")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should not be blank" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
 end
