87f9af8f61f52f427c3105f20520e496d595cb75
NoNameA 774 <nonamea774@gmail.com>
2016-01-22 02:09:55 +0900

    add user controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-#  test "should get new" do
-#    get :new
-#    assert_response :success
-#  end
+  def setup
+    @user = users(:nona)
+  end
 
+  test "should get show with name" do
+    get :show, name: @user.name
+    assert_response :success
+  end
+
+  test "should get show with name(format: json)" do
+    get :show, name: @user.name, format: :json
+    assert_response :success
+    json = JSON.parse(response.body)
+    assert_equal @user.name, json["name"]
+    assert_not_nil json["created_at"]
+  end
 end
