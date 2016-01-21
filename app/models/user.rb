7248bfc9634b858ecec764d20c61e7b02f244a57
NoNameA 774 <nonamea774@gmail.com>
2016-01-22 01:52:41 +0900

    add digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,10 @@ class User < ActiveRecord::Base
   def to_param  # overridden
     name
   end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
