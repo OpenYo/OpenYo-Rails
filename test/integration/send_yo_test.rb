1d5d82616829924325c66bb3eb5a835af3528229
NoNameA 774 <nonamea774@gmail.com>
2016-01-23 23:15:18 +0900

    add unauthorized test

--- test/integration/send_yo_test.rb
+++ test/integration/send_yo_test.rb
@@ -13,8 +13,18 @@ class SendYoTest < ActionDispatch::IntegrationTest
       post yo_path(@other), { format: :json }, { "X-API-TOKEN": token }
     end
     assert_template 'yos/sent_yo'
-    assert_equal 201, response.status
+    assert_response :created
     json = JSON.parse(response.body)
     assert { json["text"] == "sent Yo!" }
+  end
+
+  test "send yo to other without any token" do
+    assert_no_difference [ '@other.yos.count'] do
+      post yo_path(@other), { format: :json }
+    end
+    assert_template 'application/authentication_required'
+    assert_response :unauthorized
+    json = JSON.parse(response.body)
+    assert { json["text"] == "unauthorized" }
   end
 end
