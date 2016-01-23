929e628260492505cfbdc41a83de5abcf3a7839c
NoNameA 774 <nonamea774@gmail.com>
2016-01-24 04:34:38 +0900

    impl create api_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   include ApiKeysHelper
+  include ApplicationHelper
 
   private
   def token_auth
     unless current_user then
+      render :authentication_required, status: :unauthorized
+      return false
+    end
+  end
+
+  def password_auth
+    unless password_current_user then
       render :authentication_required, status: :unauthorized
       return false
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def password_current_user
+    return @pass_current_user if @pass_current_user
+    return false if params[:user].nil?
+    user = User.find_by(name: params[:user][:name])
+
+    if user && user.authenticate(params[:user][:password])
+      @user = user
+    else
+      false
+    end
+  end
 end
