1e484f6e288d5fd1faf9f790bf3ae4abb9040750
NoNameA 774 <nonamea774@gmail.com>
2016-01-24 04:52:14 +0900

    add test

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   post '/yo/:to', :to => 'yos#create', :as => :yo
 
-  post '/tokens/', :to => 'users#create_token'
+  post '/tokens/', :to => 'users#create_token', :as => :token
 
   root 'static_pages#home'
 
