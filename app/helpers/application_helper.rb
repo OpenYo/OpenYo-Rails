9f978be4926fd9207c8f21f61ca0981aaf299fe3
NoNameA 774 <nonamea774@gmail.com>
2016-01-25 17:32:29 +0900

    impl previous test(use params[:name] instead of params[:user][:name])

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,9 @@
 module ApplicationHelper
   def password_current_user
     return @pass_current_user if @pass_current_user
-    return false if params[:user].nil?
-    user = User.find_by(name: params[:user][:name])
+    user = User.find_by(name: params[:name])
 
-    if user && user.authenticate(params[:user][:password])
+    if user && user.authenticate(params[:password])
       @user = user
     else
       false
