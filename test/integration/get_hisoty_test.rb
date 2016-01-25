6cc8afb4773a48acd2bf8cd4f36298e981647ea6
NoNameA 774 <nonamea774@gmail.com>
2016-01-25 22:19:32 +0900

    break and add test

--- test/integration/get_hisoty_test.rb
+++ test/integration/get_hisoty_test.rb
@@ -13,7 +13,10 @@ class GetHisotyTest < ActionDispatch::IntegrationTest
     get history_path, { format: :json }, { "X-API-TOKEN": @other_token }
     assert_response :ok
     json = response_json
-    assert { json["count"] == 1 and json["count"] == json["history"].count }
+    assert { json["count"] == 1 }
+    assert { not json["history"].nil? }
+    assert { json["history"].count == 1 }
     assert { json["history"][0]["user"] == @user.name }
+    assert { not json["history"][0]["yoed_at"].nil? }
   end
 end
